e65b94f8cae738f10d31608f95a30267fba1eb40
miyakei1225 <miyakeionigiri@yahoo.co.jp>
2022-11-03 13:30:17 +0900

    add: bottom bar menu and icons

--- types.tsx
+++ types.tsx
@@ -3,12 +3,9 @@
  * https://reactnavigation.org/docs/typescript/
  */
 
-import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
-import {
-  CompositeScreenProps,
-  NavigatorScreenParams,
-} from "@react-navigation/native";
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { BottomTabScreenProps } from '@react-navigation/bottom-tabs'
+import { CompositeScreenProps, NavigatorScreenParams } from '@react-navigation/native'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
 
 declare global {
   namespace ReactNavigation {
@@ -17,22 +14,27 @@ declare global {
 }
 
 export type RootStackParamList = {
-  Root: NavigatorScreenParams<RootTabParamList> | undefined;
-  Modal: undefined;
-  NotFound: undefined;
-};
+  Root: NavigatorScreenParams<RootTabParamList> | undefined
+  Modal: undefined
+  NotFound: undefined
+}
 
-export type RootStackScreenProps<Screen extends keyof RootStackParamList> =
-  NativeStackScreenProps<RootStackParamList, Screen>;
+export type RootStackScreenProps<Screen extends keyof RootStackParamList> = NativeStackScreenProps<
+  RootStackParamList,
+  Screen
+>
 
 export type RootTabParamList = {
-  TabOne: undefined;
-  TabTwo: undefined;
-  NotFound: undefined;
-};
+  TabOne: undefined
+  TabTwo: undefined
+  Post: undefined
+  Purchase: undefined
+  Recommend: undefined
+  Record: undefined
+  Search: undefined
+}
 
-export type RootTabScreenProps<Screen extends keyof RootTabParamList> =
-  CompositeScreenProps<
-    BottomTabScreenProps<RootTabParamList, Screen>,
-    NativeStackScreenProps<RootStackParamList>
-  >;
+export type RootTabScreenProps<Screen extends keyof RootTabParamList> = CompositeScreenProps<
+  BottomTabScreenProps<RootTabParamList, Screen>,
+  NativeStackScreenProps<RootStackParamList>
+>
